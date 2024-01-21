6d0b9992b476cd7f4cba34d9051379a4a3e56862
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-21 22:43:36 +0800

    Done defining the function to display even indices vertically

--- Even_Index_Chars.py
+++ Even_Index_Chars.py
@@ -1,4 +1,11 @@
 # Print characters from a string that are present at an even index number
+
 # pseudocode
+
 # Define the function to display even indices vertically
+def display_even_indices_vertically(input_str):
+    even_indices_chars=input_str[::2]
+    for char in even_indices_chars:
+        print(char)
+
 # Print and display the result
